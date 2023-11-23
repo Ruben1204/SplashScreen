dc30daa5e1e7d33a673f055f2b3a33642107d53d
Ruben Rueda <ingeniero.rubenrueda@gmail.com>
2023-11-23 09:44:04 -0500

    FirstCommit
    Implementation SplashScreen on NETMAUI

---
+++ PantallaDeBienvenida/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PantallaDeBienvenida
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PantallaDeBienvenida/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PantallaDeBienvenida
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
